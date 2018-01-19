7e99df40c5ca8b33523d67c9b20ec2a0aee17b51
Hendi Santika <hendisantika@gmail.com>
2018-01-19 07:05:26 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springsecurity5/SpringSecurity5Application.java
@@ -0,0 +1,12 @@
+package com.hendisantika.springsecurity5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurity5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurity5Application.class, args);
+    }
+}
---
+++ src/test/java/com/hendisantika/springsecurity5/SpringSecurity5ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hendisantika.springsecurity5;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringSecurity5ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
